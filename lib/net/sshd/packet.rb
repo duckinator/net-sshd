3749aa0725be8b64919d2ff9f777d2b0fe92151e
Nick Markwell <nick@duckinator.net>
2013-04-14 19:14:21 -0400

    Net::SSHD::Packet is now based on Net::SSH::Packet.

--- lib/net/sshd/packet.rb
+++ lib/net/sshd/packet.rb
@@ -2,11 +2,10 @@ require 'net/ssh'
 
 module Net
   module SSHD
-    class Packet < Net::SSH::Buffer
-      attr_accessor :type, :mac
+    class Packet < Net::SSH::Packet
+      attr_accessor :mac
 
       def initialize(buffer, mac_length)
-        super()
         buffer = Net::SSH::Buffer.new.append(buffer)
         pkt_length = buffer.read_long
         pad_length = buffer.read_byte
@@ -15,9 +14,7 @@ module Net
         padding    = buffer.read(pad_length)
         @mac       = buffer.read(mac_length)
 
-        append(payload)
-
-        @type = read_byte
+        super(payload)
       end
 
       def read_list
