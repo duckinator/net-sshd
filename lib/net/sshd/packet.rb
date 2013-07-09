39cc68f43e8d64116bb4b12d853631b73680c99b
Nick Markwell <nick@duckinator.net>
2013-07-09 00:00:36 -0400

    Add Net::SSHD::Packet#payload.

--- lib/net/sshd/packet.rb
+++ lib/net/sshd/packet.rb
@@ -3,14 +3,14 @@ require 'net/ssh'
 module Net
   module SSHD
     class Packet < Net::SSH::Packet
-      attr_accessor :mac
+      attr_accessor :mac, :payload
 
       def initialize(buffer, mac_length)
         buffer = Net::SSH::Buffer.new.append(buffer)
         pkt_length = buffer.read_long
         pad_length = buffer.read_byte
         pay_length = pkt_length - pad_length
-        payload    = buffer.read(pay_length)
+        @payload   = buffer.read(pay_length)
         padding    = buffer.read(pad_length)
         @mac       = buffer.read(mac_length)
 
